3b4670c466fc1751ecbc1b10f80c9028dd5f438a
VlasenkoHelena <33989161+VlasenkoElena@users.noreply.github.com>
2025-12-11 14:48:28 +0100

    Add catplot and heatmap images for medical data visualization assignment

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,55 +4,72 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
+df['height'] = df['height'].astype(int)
+df['weight'] = df['weight'].astype(int)
 
 # 2
-df['overweight'] = None
+df['overweight'] = ((df['weight'] / (df['height']/100)**2) > 25).astype(int)
 
 # 3
-
+df["cholesterol"] = np.where(df["cholesterol"] == 1, 0, 1)
+df["gluc"] = np.where(df["gluc"] == 1, 0, 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(
+        df,
+        id_vars=['cardio'],
+        value_vars=['active','alco','cholesterol','gluc','overweight','smoke']
+    )
 
+    catplot = sns.catplot(
+        data=df_cat,
+        x='variable',
+        hue='value',
+        col='cardio',
+        kind='count'
+    )
 
-    # 6
-    df_cat = None
-    
+    catplot.set_axis_labels('variable', 'total')
+    fig = catplot.fig
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
     fig.savefig('catplot.png')
     return fig
 
 
 # 10
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Clean the data
+    df_heat = \
+        df[(df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))]
 
-    # 12
-    corr = None
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(df_heat.corr(), dtype=bool))
 
 
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots()
 
-    # 14
-    fig, ax = None
-
-    # 15
-
+    # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(data=corr, 
+                annot=True, 
+                fmt=".1f", 
+                linewidth=.5, 
+                mask=mask, 
+                annot_kws={'fontsize':6}, 
+                cbar_kws={"shrink": .7}, 
+                square=False, 
+                center=0, 
+                vmax=0.30);
 
 
     # 16
